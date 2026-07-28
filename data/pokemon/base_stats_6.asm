4d0eba707d0be9f898692ad79f42325ca4c1cbd9
vuvg1991 <167320426+vuvg1991@users.noreply.github.com>
2026-07-27 22:58:13 -0600

    Uncomment includes for additional Pokémon base stats

--- data/pokemon/base_stats_6.asm
+++ data/pokemon/base_stats_6.asm
@@ -3,9 +3,9 @@ SECTION "Base Stats 6", ROMX
 BaseData6:
 INCLUDE "data/pokemon/base_stats/decidueye.asm"   ; DECIDUEYE
 INCLUDE "data/pokemon/base_stats/decidueye_h.asm" ; DECIDUEYE_H
-; INCLUDE "data/pokemon/base_stats/pikipek.asm"   ; 
-; INCLUDE "data/pokemon/base_stats/trumbeak.asm"  ; 
-; INCLUDE "data/pokemon/base_stats/toucannon.asm" ; 
+INCLUDE "data/pokemon/base_stats/pikipek.asm"     ; PIKIPEK
+INCLUDE "data/pokemon/base_stats/trumbeak.asm"    ; TRUMBEAK
+INCLUDE "data/pokemon/base_stats/toucannon.asm"   ; TOUCANNON
 INCLUDE "data/pokemon/base_stats/yungoos.asm"     ; YUNGOOS
 INCLUDE "data/pokemon/base_stats/gumshoos.asm"    ; GUMSHOOS
 INCLUDE "data/pokemon/base_stats/grubbin.asm"     ; GRUBBIN
@@ -18,10 +18,10 @@ INCLUDE "data/pokemon/base_stats/ribombee.asm"    ; RIBOMBEE
 INCLUDE "data/pokemon/base_stats/rockruff.asm"    ; ROCKRUFF
 INCLUDE "data/pokemon/base_stats/lycanroc_md.asm" ; LYCANROC_MD
 INCLUDE "data/pokemon/base_stats/lycanroc_mn.asm" ; LYCANROC_MN
-; INCLUDE "data/pokemon/base_stats/mareanie.asm"  ; 
-; INCLUDE "data/pokemon/base_stats/toxapex.asm"   ; 
-; INCLUDE "data/pokemon/base_stats/mudbray.asm"   ; 
-; INCLUDE "data/pokemon/base_stats/mudsdale.asm"  ; 
+INCLUDE "data/pokemon/base_stats/mareanie.asm"    ; MAREANIE
+INCLUDE "data/pokemon/base_stats/toxapex.asm"     ; TOXAPEX
+INCLUDE "data/pokemon/base_stats/mudbray.asm"     ; MUDBRAY
+INCLUDE "data/pokemon/base_stats/mudsdale.asm"    ; MUDSDALE
 INCLUDE "data/pokemon/base_stats/dewpider.asm"    ; DEWPIDER
 INCLUDE "data/pokemon/base_stats/araquanid.asm"   ; ARAQUANID
 INCLUDE "data/pokemon/base_stats/fomantis.asm"    ; FOMANTIS
@@ -31,4 +31,4 @@ INCLUDE "data/pokemon/base_stats/shiinotic.asm"   ; SHIINOTIC
 INCLUDE "data/pokemon/base_stats/stufful.asm"     ; STUFFUL
 INCLUDE "data/pokemon/base_stats/bewear.asm"      ; BEWEAR
 INCLUDE "data/pokemon/base_stats/oranguru.asm"    ; ORANGURU
-; INCLUDE "data/pokemon/base_stats/passimian.asm" ; 
+INCLUDE "data/pokemon/base_stats/passimian.asm"   ; PRSSIMIAN
